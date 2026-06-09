a1de3ac5be6f11bdd403e38a2aab43f75a8872b3
Yevhen Onoprienko <fromzpcity@gmail.com>
2026-06-09 22:52:07 +0300

    11. usefull-things-471-barber. GSAP.

--- assets/js/main.js
+++ assets/js/main.js
@@ -95,3 +95,108 @@ const scrollActive = () => {
     });
 };
 window.addEventListener("scroll", scrollActive);
+
+//gsap
+const reveal = (selector, options = []) => {
+    gsap.from(selector, {
+        scrollTrigger: selector,
+        opacity: 0,
+        duration: 1,
+        y: 100,
+        delay: 0.3,
+        ease: "power2.out",
+        ...options,
+    });
+};
+
+const tl = gsap.timeline({});
+
+tl.fromTo(
+    ".home__bg, .home__shadow",
+    {
+        y: -800,
+        scale: 0.3,
+        opacity: 0,
+    },
+    {
+        y: 0,
+        scale: 0.3,
+        opacity: 1,
+        duration: 1,
+        ease: "power3.out",
+    }
+);
+
+tl.to(".home__bg, .home__shadow", {
+    scale: 1,
+    duration: 1,
+    ease: "back.out(0.5)",
+});
+
+tl.to(".home__bg", {
+    scale: 1.08,
+    duration: 8,
+    ease: "power1.inOut",
+    repeat: -1,
+    yoyo: true,
+    transformOrigin: "center center",
+});
+
+reveal(".home__logo", {
+    y: 0,
+    scale: 0.3,
+    delay: 1.9,
+    ease: "elastic.out(0.8,0.5)",
+});
+
+reveal(".home__title", { delay: 2.2 });
+reveal(".home__description", { delay: 2.5 });
+reveal(".home__data .button", { delay: 2.8 });
+
+if (window.scrollY < 100) {
+    reveal(".nav > *", { delay: 1.6, y: -30 });
+} else {
+    gsap.set(".nav > *", { opacity: 1, y: 0 });
+}
+
+//about
+reveal(".about__data > *", { stagger: 0.2 });
+reveal(".about__img", { delay: 0.9 });
+
+const aboutCounter = document.querySelectorAll(".about__counter");
+aboutCounter.forEach((el) => {
+    gsap.from(el, {
+        textContent: 0,
+        duration: 3,
+        ease: "power1.out",
+        snap: { textContent: 1 },
+        scrollTrigger: { trigger: el, once: true },
+    });
+});
+
+//work
+reveal(".work__data .section__title", {});
+reveal(".work__description", { delay: 0.6 });
+reveal(".work__data .swiper__pagination", { delay: 0.9 });
+reveal(".work__data :is(.swiper-button-prev, .swiper-button-next)", { delay: 1.2 });
+reveal(".work__swiper", { delay: 0.9 });
+
+//service
+reveal(".service__data .section__title", {});
+reveal(".service__plan", { delay: 0.6, stagger: 0.2 });
+reveal(".service__swiper", { delay: 0.9, stagger: 0.2 });
+
+//expert
+reveal(".expert .section__title", {});
+reveal(".expert__description", { delay: 0.6 });
+reveal(".expert__card", { delay: 0.9, stagger: 0.2 });
+
+//contact
+reveal(".contact__data .section__title", {});
+reveal(".contact__description", { delay: 0.6 });
+reveal(".contact__data .button", { delay: 0.9, y: 0, scale: 0 });
+reveal(".contact__map", { delay: 0.9 });
+reveal(".contact__card", { delay: 1.2, stagger: 0.2 });
+
+//footer
+reveal(".footer__container", {});
